5762d7cc36b2031690a9fa83c1d423ca24e100f7
bharathkumarmurugan <mail.bharath92@gmail.com>
2020-09-29 13:18:28 +0000

    Added output to create-github-repo

---
+++ create-github-repo/output.tf
@@ -0,0 +1,9 @@
+output "html_clone_url" {
+    value = github_repository.terraform-repo.http_clone_url
+}
+output "git_clone_url" {
+    value = github_repository.terraform-repo.git_clone_url
+}
+output "ssh_clone_url" {
+    value = github_repository.terraform-repo.ssh_clone_url
+}
