e925bfcbaaa27eee815fca47c9e7ee3bafdbfdaa
bharathkumarmurugan <mail.bharath92@gmail.com>
2020-10-07 18:35:58 +0530

    load-balancer output updated

--- load-balancer/modules/ec2/output.tf
+++ load-balancer/modules/ec2/output.tf
@@ -1,17 +1,17 @@
-output "instance_public_ip" {
-  value = zipmap(
-    aws_instance.example.*.tags.Name,
-    coalescelist(aws_instance.example.*.public_ip),
-  )
-  description = "Map of Instances to Public IPs"
-}
-output "instance_private_ip" {
-  value = zipmap(
-    aws_instance.example.*.tags.Name,
-    coalescelist(aws_instance.example.*.private_ip),
-  )
-  description = "Map of Instances to Private IPs"
-}
+# output "instance_public_ip" {
+#   value = zipmap(
+#     aws_instance.example.*.tags.Name,
+#     coalescelist(aws_instance.example.*.public_ip),
+#   )
+#   description = "Map of Instances to Public IPs"
+# }
+# output "instance_private_ip" {
+#   value = zipmap(
+#     aws_instance.example.*.tags.Name,
+#     coalescelist(aws_instance.example.*.private_ip),
+#   )
+#   description = "Map of Instances to Private IPs"
+# }
 output "ins-pub-ip" {
   value = aws_instance.example.public_ip
 }
